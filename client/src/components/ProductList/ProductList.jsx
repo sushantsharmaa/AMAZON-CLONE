5b39dc723056fb7e6e5557176f249952bb4610b1
Sushant Sharma <sushant.x01@gmail.com>
2022-05-13 16:28:16 +0530

    Low-to-high and high-to-low functionality added to product listing page

--- client/src/components/ProductList/ProductList.jsx
+++ client/src/components/ProductList/ProductList.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import Product from './Product';
 import "./productlist.css"
 
 const ProductList = () => {
     const { products } = useSelector((state) => state.productData);
+    const [lowAndHigh, setLowAndHigh] = useState("");
+
     return (
         <div className='plist'>
             <hr />
             <div className='sort'>
                 <h6>MOBILES</h6>
-                <select>
+                <select onChange={(e) => setLowAndHigh(e.target.value)}>
                     <option>Sort By: Featured</option>
-                    <option>Low to High</option>
-                    <option>High to Low</option>
+                    <option value="low-high">Low to High</option>
+                    <option value="high-low">High to Low</option>
                 </select>
             </div>
             <hr />
@@ -24,39 +26,49 @@ const ProductList = () => {
                     <hr />
                     <h6>Brand</h6>
                     <input type="checkbox" name="oneplus" />
-                    <label style={{ marginLeft: "5px" }} for="brand">OnePlus</label>
+                    <label style={{ marginLeft: "5px" }} >OnePlus</label>
                     <br />
                     <input type="checkbox" name="xiaomi" />
-                    <label style={{ marginLeft: "5px" }} for="brand">Xiaomi</label>
+                    <label style={{ marginLeft: "5px" }} >Xiaomi</label>
                     <br />
                     <input type="checkbox" name="iQOO" />
-                    <label style={{ marginLeft: "5px" }} for="brand">iQOO</label>
+                    <label style={{ marginLeft: "5px" }} >iQOO</label>
                     <br />
                     <input type="checkbox" name="realme" />
-                    <label style={{ marginLeft: "5px" }} for="brand">Realme</label>
+                    <label style={{ marginLeft: "5px" }} >Realme</label>
                     <br />
                     <input type="checkbox" name="samsung" />
-                    <label style={{ marginLeft: "5px" }} for="brand">Samsung</label>
+                    <label style={{ marginLeft: "5px" }} >Samsung</label>
                     <br />
                     <h6 style={{ marginTop: "15px" }}>Price</h6>
                     <input type="checkbox" name="price" />
-                    <label style={{ marginLeft: "5px" }} for="price">Under ₹1,000</label>
+                    <label style={{ marginLeft: "5px" }} >Under ₹1,000</label>
                     <br />
                     <input type="checkbox" name="price" />
-                    <label style={{ marginLeft: "5px" }} for="price">₹1,000 - ₹5,000</label>
+                    <label style={{ marginLeft: "5px" }} >₹1,000 - ₹5,000</label>
                     <br />
                     <input type="checkbox" name="price" />
-                    <label style={{ marginLeft: "5px" }} for="price">₹5,000 - ₹10,000</label>
+                    <label style={{ marginLeft: "5px" }} >₹5,000 - ₹10,000</label>
                     <br />
                     <input type="checkbox" name="price" />
-                    <label style={{ marginLeft: "5px" }} for="price">₹10,000 - ₹20,000</label>
+                    <label style={{ marginLeft: "5px" }} >₹10,000 - ₹20,000</label>
                     <br />
                     <input type="checkbox" name="price" />
-                    <label style={{ marginLeft: "5px" }} for="price">Over ₹20,000</label>
+                    <label style={{ marginLeft: "5px" }} >Over ₹20,000</label>
 
                 </div>
                 <div className='plist-products'>
-                    {products.map((item) => {
+                    {products && products.filter((item) => {
+                        return item.price > 0
+                    }).sort((a, b) => {
+                        if (lowAndHigh === "low-high") {
+                            return a.price - b.price;
+                        }
+                        if (lowAndHigh === "high-low") {
+                            return b.price - a.price;
+                        }
+                        return 0;
+                    }).map((item) => {
                         return <Product key={item.id} {...item} />
                     })}
                 </div>
