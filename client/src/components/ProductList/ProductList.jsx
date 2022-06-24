29daa2391a555049c9a7767091277cd8eb98d27a
Sushant Sharma <sushant.x01@gmail.com>
2022-06-24 22:02:09 +0530

    Row three and four added on landing page

--- client/src/components/ProductList/ProductList.jsx
+++ client/src/components/ProductList/ProductList.jsx
@@ -1,90 +1,143 @@
-import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom';
-import Product from './Product';
-import "./productlist.css"
+import "./productlist.css";
+import Product from "./Product";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const ProductList = () => {
-    const { products } = useSelector((state) => state.productData);
-    const [lowAndHigh, setLowAndHigh] = useState("");
-    const [brand, setBrand] = useState("");
-    const [price, setPrice] = useState(0);
+  const { products } = useSelector((state) => state.productData);
+  const [lowAndHigh, setLowAndHigh] = useState("");
+  const [brand, setBrand] = useState("");
+  const [price, setPrice] = useState(0);
 
-    return (
-        <div className='plist'>
-            <hr />
-            <div className='sort'>
-                <h6>MOBILES</h6>
-                <select onChange={(e) => setLowAndHigh(e.target.value)}>
-                    <option>Sort By: Featured</option>
-                    <option value="low-high">Low to High</option>
-                    <option value="high-low">High to Low</option>
-                </select>
-            </div>
-            <hr />
-            <div className='plist-main'>
-                <div className='filters'>
-                    <hr />
-                    <h6>FILTERS</h6>
-                    <hr />
-                    <h6>Brand</h6>
-                    <input type="checkbox" value="OnePlus" onChange={(e) => setBrand(e.target.value)} />
-                    <label style={{ marginLeft: "5px" }} >OnePlus</label>
-                    <br />
-                    <input type="checkbox" value="Redmi" onChange={(e) => setBrand(e.target.value)} />
-                    <label style={{ marginLeft: "5px" }} >Redmi</label>
-                    <br />
-                    <input type="checkbox" value="iQOO" onChange={(e) => setBrand(e.target.value)} />
-                    <label style={{ marginLeft: "5px" }} >iQOO</label>
-                    <br />
-                    <input type="checkbox" value="Realme" onChange={(e) => setBrand(e.target.value)} />
-                    <label style={{ marginLeft: "5px" }}  >Realme</label>
-                    <br />
-                    <input type="checkbox" value="Samsung" onChange={(e) => setBrand(e.target.value)} />
-                    <label style={{ marginLeft: "5px" }}  >Samsung</label>
-                    <br />
-                    <h6 style={{ marginTop: "15px" }}>Price</h6>
-                    <input type="checkbox" value="1000" onChange={(e) => setPrice(e.target.value)} />
-                    <label style={{ marginLeft: "5px" }}  >Under ₹1,000</label>
-                    <br />
-                    <input type="checkbox" value="5000" onChange={(e) => setPrice(e.target.value)} />
-                    <label style={{ marginLeft: "5px" }}  >₹1,000 - ₹5,000</label>
-                    <br />
-                    <input type="checkbox" value="10000" onChange={(e) => setPrice(e.target.value)} />
-                    <label style={{ marginLeft: "5px" }} >₹5,000 - ₹10,000</label>
-                    <br />
-                    <input type="checkbox" value="20000" onChange={(e) => setPrice(e.target.value)} />
-                    <label style={{ marginLeft: "5px" }} >₹10,000 - ₹20,000</label>
-                    <br />
-                    <input type="checkbox" value="20001" onChange={(e) => setPrice(e.target.value)} />
-                    <label style={{ marginLeft: "5px" }} >Over ₹20,000</label>
-                </div>
-                <div className='plist-products'>
-                    {products && products.filter((item) => {
-                        return item.title.includes(brand);
-                    }).filter((item) => {
-                        if (price === 0) {
-                            return item.price > 0
-                        } else if (Number(price) > 20000) {
-                            return item.price > Number(price)
-                        }
-                        return item.price < Number(price)
-                    }).sort((a, b) => {
-                        if (lowAndHigh === "low-high") {
-                            return a.price - b.price;
-                        }
-                        if (lowAndHigh === "high-low") {
-                            return b.price - a.price;
-                        }
-                        return 0;
-                    }).map((item) => {
-                        let route = `/products/${item.id}`
-                        return <Link key={item.id} style={{ textDecoration: "none", color: "black" }} to={route}><Product {...item} /></Link>
-                    })}
-                </div>
-            </div>
+  return (
+    <div className="plist">
+      <hr />
+      <div className="sort">
+        <h6>MOBILES</h6>
+        <select onChange={(e) => setLowAndHigh(e.target.value)}>
+          <option>Sort By: Featured</option>
+          <option value="low-high">Low to High</option>
+          <option value="high-low">High to Low</option>
+        </select>
+      </div>
+      <hr />
+      <div className="plist-main">
+        <div className="filters">
+          <hr />
+          <h6>FILTERS</h6>
+          <hr />
+          <h6>Brand</h6>
+          <input
+            type="checkbox"
+            value="OnePlus"
+            onChange={(e) => setBrand(e.target.value)}
+          />
+          <label style={{ marginLeft: "5px" }}>OnePlus</label>
+          <br />
+          <input
+            type="checkbox"
+            value="Redmi"
+            onChange={(e) => setBrand(e.target.value)}
+          />
+          <label style={{ marginLeft: "5px" }}>Redmi</label>
+          <br />
+          <input
+            type="checkbox"
+            value="iQOO"
+            onChange={(e) => setBrand(e.target.value)}
+          />
+          <label style={{ marginLeft: "5px" }}>iQOO</label>
+          <br />
+          <input
+            type="checkbox"
+            value="Realme"
+            onChange={(e) => setBrand(e.target.value)}
+          />
+          <label style={{ marginLeft: "5px" }}>Realme</label>
+          <br />
+          <input
+            type="checkbox"
+            value="Samsung"
+            onChange={(e) => setBrand(e.target.value)}
+          />
+          <label style={{ marginLeft: "5px" }}>Samsung</label>
+          <br />
+          <h6 style={{ marginTop: "15px" }}>Price</h6>
+          <input
+            type="checkbox"
+            value="1000"
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          <label style={{ marginLeft: "5px" }}>Under ₹1,000</label>
+          <br />
+          <input
+            type="checkbox"
+            value="5000"
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          <label style={{ marginLeft: "5px" }}>₹1,000 - ₹5,000</label>
+          <br />
+          <input
+            type="checkbox"
+            value="10000"
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          <label style={{ marginLeft: "5px" }}>₹5,000 - ₹10,000</label>
+          <br />
+          <input
+            type="checkbox"
+            value="20000"
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          <label style={{ marginLeft: "5px" }}>₹10,000 - ₹20,000</label>
+          <br />
+          <input
+            type="checkbox"
+            value="20001"
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          <label style={{ marginLeft: "5px" }}>Over ₹20,000</label>
         </div>
-    )
-}
+        <div className="plist-products">
+          {products &&
+            products
+              .filter((item) => {
+                return item.title.includes(brand);
+              })
+              .filter((item) => {
+                if (price === 0) {
+                  return item.price > 0;
+                } else if (Number(price) > 20000) {
+                  return item.price > Number(price);
+                }
+                return item.price < Number(price);
+              })
+              .sort((a, b) => {
+                if (lowAndHigh === "low-high") {
+                  return a.price - b.price;
+                }
+                if (lowAndHigh === "high-low") {
+                  return b.price - a.price;
+                }
+                return 0;
+              })
+              .map((item) => {
+                let route = `/products/${item.id}`;
+                return (
+                  <Link
+                    key={item.id}
+                    style={{ textDecoration: "none", color: "black" }}
+                    to={route}
+                  >
+                    <Product {...item} />
+                  </Link>
+                );
+              })}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ProductList
+export default ProductList;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,34 +1,60 @@
 import "./home.css";
-import { rowOne } from "./data";
+import { rowOne, rowTwo } from "./data";
+import { useSelector } from "react-redux";
 import Card from "../../components/Card/Card";
 import Carousel from "../../components/Carousel/Carousel";
 
 const Home = () => {
+  const { products } = useSelector((state) => state.productData);
+  console.log(products);
   return (
     <div className="home">
       <Carousel />
-      <div className="product-field">
-        <div className="home__row">
-          {rowOne.map((item) => {
-            return <Card key={item.id} {...item} />;
-          })}
-          <div className="last_card">
-            <div id="last_card_one">
-              <h5>Shop on the Amazon App</h5>
-              <p>
-                Fast, convenient and secure | Over 17 crore products in your
-                pocket
-              </p>
-              <span>Download the Amazon App</span>
+      <div className="products-category">
+        <div className="row-one">
+          <div className="row-one-left">
+            {rowOne.map((item) => {
+              return <Card key={item.id} {...item} />;
+            })}
+            <div className="row-one-right">
+              <div id="last_card_one">
+                <h5>Shop on the Amazon App</h5>
+                <p>
+                  Fast, convenient and secure | Over 17 crore products in your
+                  pocket
+                </p>
+                <span>Download the Amazon App</span>
+              </div>
+              <div id="last_card_two">
+                <img
+                  src="https://m.media-amazon.com/images/I/71zorsANP+L._SL1500_.jpg"
+                  alt="Havells"
+                />
+                <h6>Havells HC4085 - 1000 W Air Care Styler</h6>
+                <p>₹2,149.00</p>
+              </div>
             </div>
-            <div id="last_card_two">
-              <img
-                src="https://m.media-amazon.com/images/I/71zorsANP+L._SL1500_.jpg"
-                alt="Havells"
-              />
-              <h6>Havells HC4085 - 1000 W Air Care Styler</h6>
-              <p>₹2,149.00</p>
-            </div>
+          </div>
+        </div>
+        <div className="row-two">
+          <div className="row-two-left">
+            <img
+              src="https://otakukart.com/wp-content/uploads/2021/05/Dom-Season-1-1.jpg"
+              alt="amazon-prime"
+            />
+          </div>
+          <div className="row-two-right">
+            {rowTwo.map((item) => {
+              return <Card key={item.id} {...item} />;
+            })}
+          </div>
+        </div>
+        <div className="row-three">
+          <h3>Related to items you've viewed</h3>
+          <div className="row-three-img">
+            {products.map((item) => {
+              return <img key={item.id} src={item.url} alt={item.title} />;
+            })}
           </div>
         </div>
       </div>
