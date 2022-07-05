6d60b8a1d6f1643f2a2238e371d87e3e7a388211
Sushant Sharma <sushant.x01@gmail.com>
2022-07-05 08:16:53 +0530

    Bugs Fix

--- client/src/firebase-config.js
+++ client/src/firebase-config.js
@@ -1,14 +1,14 @@
+import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBmKgjxVuuRyEXuOQ2l3JVWT1vyL-NBWM4",
-    authDomain: "clone-f288a.firebaseapp.com",
-    projectId: "clone-f288a",
-    storageBucket: "clone-f288a.appspot.com",
-    messagingSenderId: "212960916495",
-    appId: "1:212960916495:web:180f0a49135ea88de71f7a",
-    measurementId: "G-KEE2JXKG4G"
+  apiKey: "AIzaSyBmKgjxVuuRyEXuOQ2l3JVWT1vyL-NBWM4",
+  authDomain: "clone-f288a.firebaseapp.com",
+  projectId: "clone-f288a",
+  storageBucket: "clone-f288a.appspot.com",
+  messagingSenderId: "212960916495",
+  appId: "1:212960916495:web:180f0a49135ea88de71f7a",
+  measurementId: "G-KEE2JXKG4G",
 };
 
 const app = initializeApp(firebaseConfig);
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from "react-redux";
-import App from './App';
+import App from "./App";
+import React from "react";
 import { store } from "./store";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <App />
